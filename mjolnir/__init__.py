d634e8b7b8596cd0f0c5195cc6a697f566e949d3
Ben Cos <52817096+BenCos17@users.noreply.github.com>
2024-06-26 22:59:25 +0100

    Update __init__.py

--- mjolnir/__init__.py
+++ mjolnir/__init__.py
@@ -3,12 +3,13 @@ import pathlib
 
 from redbot.core.bot import Red
 
+# Ensure correct import paths
 from .mjolnir import Mjolnir
-from mjolnir import menus
+from . import menus  # Ensure this is a relative import
 
+# Load the end user data statement
 with open(pathlib.Path(__file__).parent / "info.json") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
-
 
 async def setup(bot: Red):
     bot.add_cog(Mjolnir(bot))
